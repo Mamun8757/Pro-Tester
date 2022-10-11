4f13eb10c31aa9c9ddb001c8deabc65697d3b232
Mamun8757 <almamun8757@gmail.com>
2022-10-11 11:08:02 +0600

    TailwindCss Added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.css';
 const Header = () => {
     return (
-        <nav>
+        <nav className='header'>
             <Link to='/'>Home</Link>
             <Link to='/topics'>Topics</Link>
             <Link to='/statistics'>Statistics</Link>
